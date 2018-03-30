f5ddcf421abfbfc638b4fc0820cd9f4829172365
unknown <jasonjasonjasonc@gmail.com>
2018-03-30 14:36:47 -0400

    adding pytest_test.py and testFile.py

---
+++ pytest_test.py
@@ -0,0 +1,12 @@
+import testFile
+import pytest
+import sys
+
+@pytest.mark.skip(sys.version_info > (3,5), reason = "skipping")
+def test_calc_total():
+	total = testFile.calc_total(4,5)
+	assert total == 10
+
+def test_calc_multiply():
+	result = testFile.calc_multiply(10,3)
+	assert result == 30
