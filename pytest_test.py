5507f95f4cea214473fcb0ebc5a613fef95f6bed
unknown <jasonjasonjasonc@gmail.com>
2018-03-30 14:54:53 -0400

    commented out pytest skip marker

--- pytest_test.py
+++ pytest_test.py
@@ -6,7 +6,9 @@ import sys
 def test_calc_total():
 	total = testFile.calc_total(4,5)
 	assert total == 10
+	print("test_calc_total passed")
 
 def test_calc_multiply():
 	result = testFile.calc_multiply(10,3)
 	assert result == 30
+	print ("test_calc_multiply passed")
