958e97119455558280229788a426af1a9207a7ee
unknown <jasonjasonjasonc@gmail.com>
2018-03-30 14:53:24 -0400

    commented out pytest skip marker

--- pytest_test.py
+++ pytest_test.py
@@ -2,7 +2,7 @@ import testFile
 import pytest
 import sys
 
-@pytest.mark.skip(sys.version_info > (3,5), reason = "skipping")
+# @pytest.mark.skip(sys.version_info > (3,5), reason = "skipping")
 def test_calc_total():
 	total = testFile.calc_total(4,5)
 	assert total == 10
